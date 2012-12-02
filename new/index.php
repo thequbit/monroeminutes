7650001a4177a9284a1550ade0def0c0779cb2e9
Timothy Duffy <thequbit@gmail.com>
2012-12-02 04:30:17 -0800

    added super simple web form that posts to the searchapi.php file that returns a json object

--- new/index.php
+++ new/index.php
@@ -4,28 +4,65 @@
 </head>
 <body>
 
-	<?
+	<div class="top">
 
-	require_once("OrganizationsTool.class.php");
+		<div class="searchbar"></div>
 
-	// start of select multiple
-	echo '<select multiple="multiple">';
+			<div class="header">
+			
+			<h3>Monroe Minutes</h3></br>
+			
+			</div>
+
+			<form action="searchapi.php">
+
+				Keyword Search:</br>
+					<input type="text" name="keywordsearch" size="25"><br>
+
+				Address:</br>
+					<input type="text" name="address" size="25"><br>
+
+				Date Range:</br>
+					<input type="text" name="startdate" size="8"> - <input type="text" name="enddate" size="8"></br>
+
+				Organizations:</br>
+
+				<?
+				
+					/////////////////////////////////////////////////
+					//
+					// Select Organization PHP Code
+					//
+					/////////////////////////////////////////////////
+					
+					require_once("OrganizationsTool.class.php");
+
+					// start of select multiple
+					echo '<select multiple="organization" name="organization[]">';
+					
+					// use our tool to get all of the organization names from the database
+					$orgtool = new OrganizationsTool();	
+					$orgnames = $orgtool->GetAllOrganizationNames();
+					
+					// create the select multiple object based on DB into
+					foreach($orgnames as $name)
+					{
+						echo '<option value="' . $name . '">' . $name . '</option>';
+					}
+					
+					// end of select multiple
+					echo '</select></br>';
+
+				?>
+			
+			<input type="submit">
+			
+			</form>
+		
+		</div>
+		
+	</div>
 	
-	// use our tool to get all of the organization names from the database
-	$orgtool = new OrganizationsTool();	
-	$orgnames = $orgtool->GetAllOrganizationNames();
-	
-	// create the select multiple object based on DB into
-	foreach($orgnames as $name)
-	{
-		echo '<option value="' . $name . '">' . $name . '</option>'
-	}
-	
-	// end of select multiple
-	echo '</select>'
-
-	?>
-
 </body>
 </html>
 
