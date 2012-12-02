b41afeac95bade80b9b7a6483d17a0de6ab3b0e1
Timothy Duffy <thequbit@gmail.com>
2012-12-02 10:16:21 -0800

    made changes.  deprecated.

--- new/backend/henrietta_scrapper/createtokens.py
+++ new/backend/henrietta_scrapper/createtokens.py
@@ -3,23 +3,25 @@ import os
 import json
 import sys
 
-mydirectory=os.path.split(__file__)[0]
+mydirectory=os.path.abspath(os.path.split(__file__)[0]) + "/"
+
+#print mydirectory
 
 listing = os.walk(mydirectory)
 
-print listing
+#print listing
 
 for path,folder,filelist in listing:
 
-	print path,folder,filelist
+	#print path,folder,filelist
 
 	for infile in filelist:
 	
-		print infile
+		#print infile
 
 		if not infile[-4:]==".txt":
 			continue
-		with open(infile) as file_:
+		with open(os.path.join(path,infile)) as file_:
 			tokens = nltk.word_tokenize(file_.read(-1))
 		fdist = nltk.FreqDist(word.lower() for word in tokens)
 		for token in [",","and","of","the","for","a","to","aye","nay","voting","town","supervisor","on","be","councilman", "councilmen","councilwoman","we","this","is","are","in","would","that","$","page","(",")","has","i","at","you","it","with","there","so","?","have","new","#","as",":","by","whereas","where","here","yes","no","now","from","but","not"]:
@@ -27,12 +29,14 @@ for path,folder,filelist in listing:
 				del fdist[token]
 	
 		infile = infile[:-8]
-		data=dict(suborgid=sys.argv[1], documenturl='/'.join(path, folder, infile), frequency=fdist)
+		data=dict(suborgid=sys.argv[1], documenturl='/'.join([path[len(mydirectory):], infile]), frequency=fdist)
 	
 		with open(infile + ".json", "w") as outfile:
 			json.dump(data, outfile)
 
 
+
+print "done."
 
 #	print path,folder,infile
 
