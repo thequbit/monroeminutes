f49dd02ea2554815fe27835caf612cd65104a54d
thequbit <thequbit@gmail.com>
2012-12-05 00:36:58 -0500

    added javascript code to read from searchapi.php json object and dynamicly print the results to the page in a defined <div> tag.  Note: not pretty at all.

--- src/index.php
+++ src/index.php
@@ -5,6 +5,62 @@
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 
 	<script>
+	
+		function performSearch()
+		{
+	
+			// get values from the text boxes on the page
+			var searchString = document.getElementById('searchstring').value;
+			
+			// get start time
+			var startTime = new Date().getTime();
+			
+			//alert(searchString);
+			
+			// get json from api call
+			$.getJSON("searchapi.php",
+			{
+				searchstring: searchString
+			},
+				function(data) {
+					
+					// clear out any html that may have been put there already.
+					$("div.results").html(resultsHtml);
+					
+					// init our html contents variable
+					var resultsHtml = "";
+					
+					// itterate through the returned json array and add each document to the div
+					$.each(data, 
+						function(i,item){
+							
+							//alert(item.suborgname);
+							
+							resultsHtml += "<h3><a href=\"http://" + item.sourceurl + "\">" + item.orgname + " - " + item.suborgname + "</a></h3>\n";
+							//resultsHtml += "<p><b>Suborganization Name:</b> " + item.suborgname + "</p>\n";
+							//resultsHtml += "<p><b>Organization Name:</b> " + item.orgname + "</p>\n";
+							//resultsHtml += "<p><b>Source URL:</b> " + item.sourceurl + "</p>\n";
+							
+							resultsHtml += "<p><b>Document Name:</b> " + item.name + "</p>\n";
+							resultsHtml += "<p><b>Document Publication Date:</b> " + item.date + "</p>\n";
+							//resultsHtml += "<p><b>Word:</b> " + item.word + "</p>\n";
+							//resultsHtml += "<p><b>Frequency:</b> " + item.frequency + "</p></br></br>\n";
+							
+					});
+					
+					// take current time
+					var endTime = new Date().getTime();
+					
+					// calculate how long it took for the search
+					var timeTaken = endTime - startTime;
+					
+					$("div.timetaken").html("<p>Search took: " + timeTaken + " milliseconds</p>");
+					$("div.results").html(resultsHtml);
+					
+				}
+			);
+		
+		}
 		
 	</script>
 
@@ -21,19 +77,18 @@
 			
 			</div>
 
-			<form action="searchapi.php">
+			<!--<form action="searchapi.php">-->
 
 				Keyword Search:</br>
-					<input type="text" name="keywordsearch" size="25"><br>
+					<input type="text" id="searchstring" name="searchstring" size="25"><br>
 
 				Address:</br>
-					<input type="text" name="address" size="25"><br>
+					<input type="text" id="address" name="address" size="25"><br>
 
 				Date Range:</br>
-					<input type="text" name="startdate" size="8"> - <input type="text" name="enddate" size="8"></br>
+					<input type="text" id="startdate" name="startdate" size="8"> - <input type="text" name="enddate" size="8"></br>
 
 				Organizations:</br>
-
 				<?
 				
 					/////////////////////////////////////////////////
@@ -45,7 +100,7 @@
 					require_once("OrganizationsTool.class.php");
 
 					// start of select multiple
-					echo '<select multiple="organization" name="organization[]">';
+					echo '<select multiple="organization" id="organizations" name="organization[]">';
 					
 					// use our tool to get all of the organization names from the database
 					$orgtool = new OrganizationsTool();	
@@ -62,9 +117,11 @@
 
 				?>
 			
-			<input type="submit">
+			<!-- <input type="submit" > -->
 			
-			</form>
+			<button onclick="performSearch()">Search</button>
+			
+			<!--</form>-->
 		
 		</div>
 		
@@ -74,9 +131,11 @@
 		
 		</div>
 		
-		<div class="results">
+		<div id="timetaken" class="timetaken">
 		
-			
+		</div>
+		
+		<div id="results" class="results">
 		
 		</div>
 		
