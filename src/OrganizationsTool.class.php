4f72aa857e0216448c4764f5e0239b97f2e37c80
thequbit <thequbit@gmail.com>
2012-12-05 00:40:23 -0500

    added function to return all Category names from the database.

--- src/OrganizationsTool.class.php
+++ src/OrganizationsTool.class.php
@@ -61,6 +61,46 @@
 			mysql_select_db(MYSQL_DATABASE, $chandle)
 				or die (MYSQL_DATABASE . " Database not found. " . MYSQL_USER);	// TODO: something more elegant than this
 
+			dprint("Connected to DB.");
+			
+			$query = "SELECT * FROM suborganization";
+			
+			// pull from DB
+			$result = mysql_db_query(MYSQL_DATABASE, $query)
+				or die("Failed Query of " . $query);  			// TODO: something more elegant than this
+			
+			dprint("itterating through db response and creating return array");
+			
+			// itterate through the results and populate an array
+			while($r = mysql_fetch_assoc($result)) {
+			
+				// using the sub organizations id pull it's name from the database
+				$orgName = $r['name'];
+			
+				dprint("Found: '" .$orgName ."'");
+			
+				// add the new name to the list of names
+				$retVal[] = $orgName;
+			
+			}
+			
+			return $retVal;
+		}
+	
+		function GetAllCategoryNames()
+		{
+		
+			$retVal = array();
+		
+			/*
+	
+			// connect to the mysql database server.  Constants taken from sqlcredentials.php
+			$chandle = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS)
+				or die("Connection Failure to Database");				// TODO: something more elegant than this
+
+			mysql_select_db(MYSQL_DATABASE, $chandle)
+				or die (MYSQL_DATABASE . " Database not found. " . MYSQL_USER);	// TODO: something more elegant than this
+
 			//dprint("Connected to DB.");
 			
 			$query = "SELECT * FROM suborganization";
@@ -83,6 +123,8 @@
 				$retVal[] = $orgName;
 			
 			}
+			
+			*/
 			
 			return $retVal;
 		}
