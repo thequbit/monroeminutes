801cf1520406e90808138efd4caad08daae8ddb5
thequbit <thequbit@gmail.com>
2012-12-08 17:13:38 -0500

    added google analytics to index page

--- src/index.php
+++ src/index.php
@@ -17,6 +17,20 @@
 
 	<link href="css/main.css" rel="stylesheet" type="text/css">
 
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-34576505-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+
 	<script>
 	
 		function initPage()
