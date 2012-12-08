c24791612d1274ce27bce6705007d7ad017dde01
thequbit <thequbit@gmail.com>
2012-12-08 02:09:22 -0500

    updated dashboard with site style

--- src/dashboard/dashboard.php
+++ src/dashboard/dashboard.php
@@ -2,23 +2,43 @@
 
 	// start our session so we have access to our $_SESSION variables
 	session_start();
-
 	if( isset($_SESSION['username']) == false )
 	{
 		header("Location: login.php");
 	}
-	else
-	{
-
-		echo "Welcome " . $_SESSION['username'] . "</br>";
-
-		echo '<a href="logout.php">Logout</a></br>';
-		
-		if( isset($_SESSION['isadmin']) && $_SESSION['isadmin'] == true )
-		{
-			echo '<a href="admin.php">Admin Page</a></br>';
-		}
-		
-	}
-
+	
 ?>
+
+<html>
+<head>
+	<title>Monroe Minutes Dashboard</title>
+
+	<link href="../css/main.css" rel="stylesheet" type="text/css">
+	<link href="../css/dashboard.css" rel="stylesheet" type="text/css">
+	
+</head>
+<body>
+
+	<div id="sitetop" class="sitetop">
+
+		<div id="topwrapper" class="topwrapper">
+
+			<?php
+			
+				echo "Welcome " . $_SESSION['username'] . "</br>";
+
+				echo '<a href="logout.php">Logout</a></br>';
+				
+				if( isset($_SESSION['isadmin']) && $_SESSION['isadmin'] == true )
+				{
+					echo '<a href="admin.php">Admin Page</a></br>';
+				}
+
+			?>
+		
+		</div>
+		
+	</div>
+
+</body>
+</html>
