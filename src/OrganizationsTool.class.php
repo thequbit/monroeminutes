aa9c6b11ae1b9619ce56902c6f2a465b8207cc95
thequbit <thequbit@gmail.com>
2012-12-08 20:41:21 -0500

    added functionality to pull org and suborg name

--- src/OrganizationsTool.class.php
+++ src/OrganizationsTool.class.php
@@ -1,7 +1,8 @@
 <?php
 
 	require_once("debug.php");
-	require_once("sqlcredentials.php");
+	//require_once("sqlcredentials.php");
+	require_once("DatabaseTool.class.php");
 
 	class OrganizationsTool
 	{
@@ -17,57 +18,13 @@
 			//dprint("pass: " . MYSQL_PASS);
 			//dprint("db: " . MYSQL_DATABASE);
 			
-			// connect to the mysql database server.  Constants taken from sqlcredentials.php
-			$chandle = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS)
-				or die("Connection Failure to Database");				// TODO: something more elegant than this
-
-			mysql_select_db(MYSQL_DATABASE, $chandle)
-				or die (MYSQL_DATABASE . " Database not found. " . MYSQL_USER);	// TODO: something more elegant than this
-
-			//dprint("Connected to DB.");
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
 			
-			$query = "SELECT * FROM organizations";
-			
-			// pull from DB
-			$result = mysql_db_query(MYSQL_DATABASE, $query)
-				or die("Failed Query of " . $query);  			// TODO: something more elegant than this
-			
-			//dprint("itterating through db response and creating return array");
-			
-			// itterate through the results and populate an array
-			while($r = mysql_fetch_assoc($result)) {
-			
-				// using the sub organizations id pull it's name from the database
-				$orgName = $r['name'];
-			
-				//dprint("Found: '" .$orgName ."'");
-			
-				// add the new name to the list of names
-				$retVal[] = $orgName;
-			
-			}
-			
-			return $retVal;
-		}
-	
-		function GetAllSubOrganizationNames()
-		{
-			$retVal = array();
-			
-			// connect to the mysql database server.  Constants taken from sqlcredentials.php
-			$chandle = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS)
-				or die("Connection Failure to Database");				// TODO: something more elegant than this
-
-			mysql_select_db(MYSQL_DATABASE, $chandle)
-				or die (MYSQL_DATABASE . " Database not found. " . MYSQL_USER);	// TODO: something more elegant than this
-
-			dprint("Connected to DB.");
-			
-			$query = "SELECT * FROM suborganizations";
-			
-			// pull from DB
-			$result = mysql_db_query(MYSQL_DATABASE, $query)
-				or die("Failed Query of " . $query);  			// TODO: something more elegant than this
+			// get the total number of organizations from the data base
+			$query = "SELECT name FROM organizations";
+			$result = $dbtool->Query($query,$chandle);
 			
 			dprint("itterating through db response and creating return array");
 			
@@ -77,7 +34,33 @@
 				// using the sub organizations id pull it's name from the database
 				$orgName = $r['name'];
 			
-				dprint("Found: '" .$orgName ."'");
+				// add the new name to the list of names
+				$retVal[] = $orgName;			
+			}
+			
+			// return the created array
+			return $retVal;
+		}
+	
+		function GetAllSubOrganizationNames()
+		{
+			$retVal = array();
+			
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
+			
+			// select all suborganizations
+			$query = "SELECT name FROM suborganizations";
+			$result = $dbtool->Query($query,$chandle);
+			
+			dprint("itterating through db response and creating return array");
+			
+			// itterate through the results and populate an array
+			while($r = mysql_fetch_assoc($result)) {
+			
+				// using the sub organizations id pull it's name from the database
+				$orgName = $r['name'];
 			
 				// add the new name to the list of names
 				$retVal[] = $orgName;
@@ -133,18 +116,13 @@
 		{
 			$retVal = -1;
 			
-			// connect to the mysql database server.  Constants taken from sqlcredentials.php
-			$chandle = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS)
-				or die("Connection Failure to Database");				// TODO: something more elegant than this
-
-			mysql_select_db(MYSQL_DATABASE, $chandle)
-				or die (MYSQL_DATABASE . " Database not found. " . MYSQL_USER);	// TODO: something more elegant than this
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
 			
+			// get sub org id from name
 			$query = "SELECT suborganizationid FROM suborganizations where name='" . $name . "'";
-			
-			// pull from DB
-			$result = mysql_db_query(MYSQL_DATABASE, $query)
-				or die("Failed Query of " . $query);  			// TODO: something more elegant than this
+			$result = $dbtool->Query($query,$chandle);
 			
 			// pull the id from the result
 			$retVal = mysql_result($result,0);
@@ -156,18 +134,13 @@
 		{
 			$retVal = -1;
 			
-			// connect to the mysql database server.  Constants taken from sqlcredentials.php
-			$chandle = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS)
-				or die("Connection Failure to Database");				// TODO: something more elegant than this
-
-			mysql_select_db(MYSQL_DATABASE, $chandle)
-				or die (MYSQL_DATABASE . " Database not found. " . MYSQL_USER);	// TODO: something more elegant than this
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
 			
+			// get the idea from the name via the database
 			$query = "SELECT organizationid FROM organizations where name='" . $name . "'";
-			
-			// pull from DB
-			$result = mysql_db_query(MYSQL_DATABASE, $query)
-				or die("Failed Query of " . $query);  			// TODO: something more elegant than this
+			$result = $dbtool->Query($query,$chandle);
 			
 			// pull the id from the result
 			$retVal = mysql_result($result,0);
@@ -178,10 +151,19 @@
 		function SubOrgNameFromID($suborgid)
 		{
 		
-			$retVal = "Happy Town!";
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
 		
-			// TODO: Pull suborg name from DB
+			// get the name of the suborg from the id
+			$query = "SELECT name FROM suborganizations where suborganizationid=" . $suborgid;
+			$result = $dbtool->Query($query,$chandle);
+			
+			$r = mysql_fetch_row($result);
 		
+			$retVal = $r[0];
+		
+			// return the suborg name
 			return $retVal;
 		
 		}
@@ -189,20 +171,53 @@
 		function OrgNameFromID($orgid)
 		{
 		
-			$retVal = "Happy City!";
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
 		
-			// TODO: Pull org name from DB
+			// get the name of the suborg from the id
+			$query = "SELECT name FROM organizations where organizationid=" . $orgid;
+			$result = $dbtool->Query($query,$chandle);
+			
+			$r = mysql_fetch_row($result);
 		
+			$retVal = $r[0];
+		
+			// return the suborg name
 			return $retVal;
 		
 		}
 		
+		function OrgIdFromSubOrgId($suborgid)
+		{
+			// connect to DB
+			$dbtool = new DatabaseTool();
+			$chandle = $dbtool->Connect();
+			
+			// get the name of the suborg from the id
+			$query = "SELECT organizationid FROM suborganizations where suborganizationid=" . $suborgid;
+			$result = $dbtool->Query($query,$chandle);
+			
+			// get first (only) result
+			$r = mysql_fetch_row($result);
+		
+			// pull name from result
+			$retVal = $r[0];
+		
+			// return the suborg name
+			return $retVal;
+		}
+		
 		function OrgNameFromSubOrgID($suborgid)
 		{
-			$retVal = "Happy County";
 			
-			// TODO: Pull org name from DB via suborgID
+			// get org id from suborg id
+			$orgid = $this->OrgIdFromSubOrgId($suborgid);
 			
+			// get org name from org id
+			$retVal = $this->OrgNameFromID($orgid);
+			
+			// return orgname
 			return $retVal;
 		}
 
