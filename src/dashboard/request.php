53e72483949f023ac0c3272f7341686314f7899d
thequbit <thequbit@gmail.com>
2012-12-09 23:16:07 -0500

    beginnings of the request page where people can requst for an organization/suborganization to be added to the scrap list

--- src/dashboard/request.php
+++ src/dashboard/request.php
@@ -18,19 +18,33 @@
 	<meta name="description" content="Meeting Minute Agrigator and Search Engine for Monroe County, NY">
 	<meta name="keywords" content="Monroe,Minutes,MonroeMinutes,Rochester,Meetings">
 
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="../js/orgs.js"></script>
+
 	<link href="../css/main.css" rel="stylesheet" type="text/css">
-	<link href="../css/dashboard.css" rel="stylesheet" type="text/css">
+	<link href="../css/request.css" rel="stylesheet" type="text/css">
+	
 
 </head>
-<body>
+<body onload="javascript:populateOrganizationList()">
 
-	<div id="sitetop class="sitetop">
+	<div id="sitetop" class="sitetop">
 	
 		<div id="topwrapper" class="topwrapper">
 		
+			<br><br>
+		
+			<div id="requesttitle" class="requesttitle">
+			
+				<img id="titleimg" class="titleimg" src="/media/title.png"></img>
+			
+			</div>
+		
 			<div id="requestinfo" class="requestinfo">
 			
-				test.
+				<div id="existingorgs" class="existingorgs">
+				
+				</div>
 			
 			</div>
 		
