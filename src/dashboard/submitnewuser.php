733e9a54e6b28418205e487108f6aafbfb6e5597
thequbit <thequbit@gmail.com>
2012-12-19 17:41:14 -0500

    modified page to reflect changes seen in other submit pages

--- src/dashboard/submitnewuser.php
+++ src/dashboard/submitnewuser.php
@@ -9,40 +9,49 @@
 	
 ?>
 
-<?php
+<html>
+<head>
+	<meta http-equiv="refresh" content="2; URL=dashboard.php">
+</head>
+<body>
 
-	require_once("../tools/debug.php");
-	require_once("../tools/LoginTool.class.php");
+	<?php
 
-	// decode our username and password from our POST event from our login form
-	$displayname = $_POST['displayname'];
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	if( $_POST['canlogin'] == "on" ) {
-		$canlogin = 1;
-	}
-	if( $_POST['enabled'] == "on" ) {
-		$isadmin = 1;
-	}
-	if( $_POST['isadmin'] == "on" ) {
-		$enabled = 1;
-	}
+		require_once("../tools/debug.php");
+		require_once("../tools/LoginTool.class.php");
+
+		// decode our username and password from our POST event from our login form
+		$displayname = $_POST['displayname'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		if( $_POST['canlogin'] == "on" ) {
+			$canlogin = 1;
+		}
+		if( $_POST['enabled'] == "on" ) {
+			$isadmin = 1;
+		}
+		if( $_POST['isadmin'] == "on" ) {
+			$enabled = 1;
+		}
+		
+		dprint("Display Name: " . $displayname);
+		dprint("Username: " . $username);
+		dprint("Password: " . $password);
+		dprint("Can Login?: " . $canlogin);
+		dprint("Is Admin?: " . $isadmin);
+		dprint("Enabled?: " . $enabled);
+		
+		// create a login tool object to help us add the user to the db
+		$loginTool = new LoginTool();
+
+		dprint("Adding user to DB ...");
+
+		// add the user to the database
+		$success = $loginTool->CreateUser($displayName, $username, $password, $canlogin, $isadmin, $enabled);
+		
+		dprint("Success: " . $success);
+
+	?>
 	
-	dprint("Display Name: " . $displayname);
-	dprint("Username: " . $username);
-	dprint("Password: " . $password);
-	dprint("Can Login?: " . $canlogin);
-	dprint("Is Admin?: " . $isadmin);
-	dprint("Enabled?: " . $enabled);
-	
-	// create a login tool object to help us add the user to the db
-	$loginTool = new LoginTool();
-
-	dprint("Adding user to DB ...");
-
-	// add the user to the database
-	$success = $loginTool->CreateUser($displayName, $username, $password, $canlogin, $isadmin, $enabled);
-	
-	dprint("Success: " . $success);
-
-?>
+</body>
+</html>
