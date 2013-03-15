451ef3a98d19e2d3ad8c6776482a935af8bf134e
thequbit <thequbit@gmail.com>
2013-03-15 14:51:42 -0400

    First commit of V2 web interface

---
+++ src/v2/web/_footer.php
@@ -0,0 +1,19 @@
+				</div>
+				
+			</div>
+			
+			<div class="footerwrapper">
+			
+				<div id="footer" class="footer">
+				
+					Copyright 2013 | Two Fift-Five Labs, LLC | West Henrietta, NY | <a href="https://github.com/thequbit/monroeminutes">Source Code</a>
+				
+				</div>
+			
+			</div>
+			
+		</div>
+		
+	</body>
+	
+</html>
---
+++ src/v2/web/js/search.js
@@ -0,0 +1,8 @@
+
+	function performSearch()
+	{
+	
+		var searchString = document.getElementById('searchstring').value;
+	
+		alert(searchString);
+	}
