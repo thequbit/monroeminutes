9f5dca5d6c4ebb1d41731908f565ffa03b2172da
thequbit <thequbit@gmail.com>
2013-06-02 01:59:13 -0400

    work done so far.  Need to pin down pdf decode errors

--- simple/scripts/simplescraper.py
+++ simple/scripts/simplescraper.py
@@ -4,8 +4,10 @@ import urllib
 import urllib2
 from random import randint
 from bs4 import BeautifulSoup
+from pprint import pprint
+import nltk
 
-from decodepdf import decodepdf
+from decodepdf import decodepdf,scrubtext
 
 from scrapeurls import scrapeurls
 from ignoreurls import ignoreurls
@@ -29,7 +31,7 @@ def getorgurls(orgid):
     urls = surls.geturls(orgid)
     return urls
 
-def getlinks(urlid,url,baseurl,siteurl):
+def getlinks(urlid,url,siteurl,baseurl):
     iurls = ignoreurls()
     ignorelinks = iurls.getallbyscrapeurlid(urlid)
 
@@ -37,11 +39,21 @@ def getlinks(urlid,url,baseurl,siteurl):
 
     pagelinks = []
     for pagelink in _pagelinks:
-        link = decodelink(pagelink,baseurl,siteurl)
-        pagelinks.append(link)
+        link = decodelink(pagelink,siteurl,baseurl)
+        pagelinks.append(link.lower())
 
     # diff the lists, dropping links from the ignore list
     links = [x for x in pagelinks if x not in ignorelinks]
+
+    #print "len(ignorelinks) = {0}".format(len(ignorelinks))
+    #pprint(ignorelinks)
+    #print "\n\n"
+    #print "len(pagelinks) = {0}".format(len(pagelinks))
+    #pprint(pagelinks)
+    #print "\n\n"
+    #print "len(links) = {0}".format(len(links))
+    #pprint(links)
+    #print "\n\n"
 
     return links
 
@@ -54,26 +66,40 @@ def _get_page_links(url):
 def decodelink(tag,siteurl,baseurl):
     #try:
         # make sure the URL is formatted correctly
-        print type(tag)
+        #print type(tag)
+        print "decoding: {0}".format(tag['href'])
         if len(tag['href']) >= 7 and tag['href'][0:7].lower() == "http://":
-                linkurl = tag['href']
+            linkurl = tag['href']
+            #print "1: {0}".format(linkurl)
         else:
             if len(tag['href']) >= 1 and tag['href'][0:1] == "/":
                 # absolute link
-                linkurl = siteurl + tag['href'][1:]
+                linkurl = siteurl + tag['href']
+                #print "2: {0}".format(linkurl)
             else:
                 # relative link
                 linkurl = baseurl + tag['href']
+                #print "3: {0}".format(linkurl)
     #except:
     #    linkurl = ""
         return linkurl
 
 def downloadlink(url):
-    # get the filename off of the url, then set the local file to that + a random number .pdf, and download it
-    urlfile = url[url.rfind("/")+1:]
-    _filename = "./downloads/{0}_{1}.download".format(urlfile,randint(0,1000000))
-    filename,headers = urllib.urlretrieve(url,_filename)
-    return filename
+    try:
+        # get the filename off of the url, then set the local file to that + a random number .pdf, and download it
+        urlfile = url[url.rfind("/")+1:]
+        _filename = "./downloads/{0}_{1}.download".format(urlfile,randint(0,1000000))
+        filename,headers = urllib.urlretrieve(url,_filename)
+        success = True
+    except:
+        success = False
+        filename = ""
+    return success,filename
+
+def addignore(link,scrapeurlid):
+    dt = datetime.datetime.now().date().isoformat()
+    iurls = ignoreurls()
+    iurls.add(link,dt,scrapeurlid)
 
 def getsuborg(pdfheader):
     sorgs = suborganizations()
@@ -123,29 +149,38 @@ def main(argv):
         
         # decode the base url and site url
         baseurl = url[:url.rfind("/")+1]
-        siteurl = url[:url.find("/",7)]
+        siteurl = url[:url.find("/",7)+1]
 
-        links = getlinks(urlid,url,baseurl,siteurl)
+        links = getlinks(urlid,url,siteurl,baseurl)
 
-        report("info","Working on {0} Links from URL ...".format(len(links)))
+        report("info","Working on {0} Links from `{1}`".format(len(links),url))
 
         for link in links:
             report("info","Working on URL `{0}`".format(link))
-            filename = downloadlink(link)
+            success,filename = downloadlink(link)
+            if success == False:
+                report("error","Bad Link Found, Skipping.\n")
+                continue
             success,pdftext,texthash = decodepdf(filename)
             if success == True:
                 success,suborgid = getsuborg(pdftext[:HEADERLENGTH])
                 if success == True:
                     docdate = getdocdate(pdftext[:HEADERLENGTH])
                     docname = getdocname(pdftext[:HEADERLENGTH])
-                    _tokens = nltk.word_tokenize(pdfTextScrubbed)
+                    pdftextscrubbed = scrubtext(pdftext)
+                    _tokens = nltk.word_tokenize(pdftextscrubbed)
                     tokens = nltk.FreqDist(word.lower() for word in _tokens)
-                    docid = savedoc(suborgid,orgid,link,docdate,docname,dochash,pdftext,tokens)
-                    report("info","Successfully Parsed And Added Document #{0}".format(docid))
+                    docid = savedoc(suborgid,orgid,link,docdate,docname,texthash,pdftext,tokens)
+                    report("info","Successfully Parsed And Added Document #{0}\n".format(docid))
                 else:
                     report("error","Unable to Decode Suborganization from PDF document.")
+                    report("info","Adding `{0}` to orphan list.\n")
+                    # TODO: add to orphan list
             else:
-                report("error","Unable to Parse PDF Into Text.")
+                report("warning","Unable to Parse PDF Into Text.")
+                report("info","Adding `{0}` to ignore list.\n".format(link))
+                addignore(link,urlid)
+            os.remove(filename)
 
     print "\nApplication Exiting ...\n"
 
