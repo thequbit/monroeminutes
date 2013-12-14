d7a56f401064cf934492a5edba8c9f5366a83c2b
thequbit <thequbit@gmail.com>
2013-12-14 16:34:46 -0500

    framework for barkingowl based scraper

---
+++ revC/src/scraper/monroeminutes_scraper.py
@@ -0,0 +1,19 @@
+from time import strftime
+from BarkingOwl.dispatcher.barkingowl_dispatcher import Dispatcher
+
+if __name__ == '__main__':
+    print "Starting Monroe Minutes Scraper ..."
+
+    url = {'targeturl': "http://timduffy.me/",
+           'title': "TimDuffy.Me",
+           'description': "Tim Duffy's Personal Website",
+           'maxlinklevel': 3,
+           'creationdatetime': str(strftime("%Y-%m-%d %H:%M:%S")),
+           'doctype': 'application/pdf',
+           'frequency': 2,
+          }
+
+    urls = [url]
+    dispatcher = Dispatcher(address='localhost',exchange='barkingowl')
+    dispatcher.seturls(urls)
+    dispatcher.start()
