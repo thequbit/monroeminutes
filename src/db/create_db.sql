4a79ca995111f52588f7592beaaa674ba8f32068
thequbit <thequbit@gmail.com>
2013-12-24 12:23:50 -0500

    updated db creation script to work with new scheme

--- src/db/create_db.sql
+++ src/db/create_db.sql
@@ -18,6 +18,14 @@ CREATE TABLE IF NOT EXISTS urls(
 );
 CREATE INDEX urls_urlid ON urls(urlid);
 
+CREATE TABLE IF NOT EXISTS bodies(
+    bodyid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    creationdatetime DATETIME NOT NULL
+);
+CREATE INDEX bodies_bodyid ON bodies(bodyid);
+
 CREATE TABLE IF NOT EXISTS orgs(
     orgid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name CHAR(255) NOT NULL,
@@ -25,32 +33,9 @@ CREATE TABLE IF NOT EXISTS orgs(
     creationdatetime DATETIME NOT NULL,
     matchtext TEXT NOT NULL,
     urlid INT NOT NULL,
-    FOREIGN KEY (urlid) REFERENCES url(urlid),
+    FOREIGN KEY (urlid) REFERENCES urls(urlid),
     bodyid INT NOT NULL,
     FOREIGN KEY (bodyid) REFERENCES bodies(bodyid)
 );
 CREATE INDEX orgs_orgid ON orgs(orgid);
-
-CREATE TABLE IF NOT EXISTS bodies(
-    bodyid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    creationdatatime DATETIME NOT NULL
-);
-CREATE INDEX bodies_bodyid ON bodies(bodyid);
-
-CREATE TABLE IF NOT EXISTS docs(
-    docid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    docurl TEXT NOT NULL,
-    linktext CHAR(255) NOT NULL,
-    urlid INT NOT NULL,
-    FOREIGN KEY (urlid) REFERENCES urls(urlid),
-    creationdatetime DATETIME NOT NULL,
-    pdfhash CHAR(255) NOT NULL,
-    textfilename TEXT NOT NULL,
-    pdffilename TEXT NOT NULL,
-    orgid INT NOT NULL,
-    FOREIGN KEY (orgid) REFERNCES orgs(orgid)
-);
-CREATE INDEX docs_docid ON docs(docid);
 
