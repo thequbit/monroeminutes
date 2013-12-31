25e5b28e0c2a78c9887fed9e9e1c2d7cf660b0bc
thequbit <thequbit@gmail.com>
2013-12-31 13:32:42 -0500

    adjusted search api and webpage to support changes

--- src/searchapi.py
+++ src/searchapi.py
@@ -1,5 +1,6 @@
 import elasticsearch
 import json
+import re
 
 class Search():
 
@@ -14,12 +15,87 @@ class Search():
         response['success'] = False
         response['error'] = ""
         response['count'] = 0
+        response['phrase'] = ""
         response['results'] = []
 
         if orgid !=0 and bodyid != 0:
             response['error'] = "orgid and bodyid both specified, only one can be used at a time."
             return response
 
+        # build the search query body
+        body = self.buildbody(phrase,page,orgid,bodyid)
+
+        # perform the search
+        results = self.es.search(index="monroeminutes",
+                                 body=body
+        )
+
+        # create our return object to send back
+        response['success'] = True
+        response['count'] = len(results['hits']['hits'])
+        response['phrase'] = phrase
+        response['results'] = []
+        for hit in results['hits']['hits']:
+            
+            # build preview text
+            pdftext = hit['_source']['pdftext']
+            previewtext = self.buildpreviewtext(phrase,pdftext)
+            
+            # build result response
+            response['results'].append({
+                'score': hit['_score'],
+                'docid': hit['_id'],
+                'docurl': hit['_source']['docurl'],
+                'docname': hit['_source']['docname'],
+                'scrapedatetime': hit['_source']['scrapedatetime'],
+                'linktext': hit['_source']['linktext'],
+                'targeturl': hit['_source']['targeturl'],
+                'previewtext': previewtext,
+                #'pdftext': pdftext
+            })
+
+        return response
+
+    def buildpreviewtext(self,phrase,pdftext):
+
+        BEFORE_LEN = 64
+        AFTER_LEN = 64
+
+        regexstr = "( +)?"
+        for i in range(0,len(phrase)):
+            if phrase[i] != ' ':
+                regexstr += "%s( +)?" % phrase[i]
+
+        count = 0
+        indexes = [(m.start(0)) for m in re.finditer(regexstr.lower(), pdftext.lower())]
+
+        #print "Found %i incidents of phrase" % len(indexes)
+
+        if len(indexes) == 0:
+            return ""
+
+        #print "pdftext length: {0}".format(len(pdftext))
+
+        text = ""
+        for index in indexes:
+            #print "index = %i" % index
+            if index < BEFORE_LEN:
+                beforeindex = 0
+            else:
+                beforeindex = index - BEFORE_LEN
+            #print "before index: {0}, len: {1}".format(beforeindex,AFTER_LEN)
+            preview = pdftext[beforeindex:(beforeindex+BEFORE_LEN+AFTER_LEN)]
+            preview = " ".join(preview.split(' ')[1:-1])
+            preview = preview.replace('\t','').replace('\n','').replace('\f','')
+            #print "preview text: {0}".format(preview)
+
+            text += "... {0}".format(preview)
+
+        text += " ..."
+        return text
+
+    def buildbody(self,phrase,page,orgid,bodyid):
+        
         if orgid == 0 and bodyid == 0:
             #create the search query
             body = {"size": 10,
@@ -62,38 +138,17 @@ class Search():
             if bodyid != 0:
                 body['query']['filtered']['filter']['query']['field']['bodyid'] = bodyid
 
-        #try:
-        if True:
-            # perform the search
-            results = self.es.search(index="monroeminutes",
-                                body=body
-            )
-
-            # create our return object to send back
-            response['success'] = True
-            response['count'] = len(results['hits']['hits'])
-            response['results'] = []
-            for hit in results['hits']['hits']:
-                response['results'].append({
-                    'score': hit['_score'],
-                    'docid': hit['_id'],
-                    'docurl': hit['_source']['docurl'],
-                    'docname': hit['_source']['docname'],
-                    'scrapedatetime': hit['_source']['scrapedatetime'],
-                    'linktext': hit['_source']['linktext'],
-                    'targeturl': hit['_source']['targeturl']
-                })
-
-        #except:
-            # if unsuccessfull, then we will return success = False
-        #    pass
-
-        return response
+        return body 
 
 if __name__ == '__main__':
 
     search = Search()
 
-    response = search.search('scottsville',orgid=0,bodyid=1)
+    phrase = "hello"
+    pdftext = "I don't know how many times I need to tell her that I prefere hello over a simple hi."
+
+    response = search.buildpreviewtext(phrase,pdftext)
+
+    #response = search.search('today',orgid=1,bodyid=0)
 
     print response
