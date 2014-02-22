01feba5cc2786f61ceb7c5851980d7f8386d4657
dylan <dylan@dylan-VirtualBox.(none)>
2014-02-21 19:10:48 -0500

    adding a setup to manage python dependencies

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+version = "0.0.0"
+
+setup(
+    name="MonroeMinutes",
+    version=version,
+    description="Scrapes minutes, tik tik tok",
+    classifiers=[
+        "Intended Audience :: Monroe",
+    ],
+    keywords="",
+    author="Tim Duffy",
+    author_email="dxa4481@rit.edu",
+    license="GPLv3+",
+    packages=find_packages(
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "flask",
+        "pdfminer==20110515",
+        "BeautifulSoup4",
+        "elasticsearch",
+        "python-magic",
+        "pymongo",
+	"mysql-python",
+    ],
+    #TODO: Deal with entry_points
+    #entry_points="""
+    #[console_scripts]
+    #pythong = pythong.util:parse_args
+    #"""
+)
+
